9de044cfeee87a3d13512c9f65be5e16d443cc33
cameron <cam@loudstyle.com>
2021-11-15 17:57:56 -0600

    answers

--- routes/api/answers.js
+++ routes/api/answers.js
@@ -1,17 +1,42 @@
 const express = require("express");
-const { restoreUser } = require("../../auth");
+const { requireAuth } = require("../../auth");
 const router = express.Router();
-const db = require("../db/models");
-const asyncHandler = require("../utils");
-router.get("/", function (req, res) {});
+const db = require("../../db/models");
+const { asyncHandler } = require("../utils");
+
+router.get(
+  "/test",
+  asyncHandler(async (req, res) => {
+    const answers = await db.Answer.findAll({ limit: 10 });
+    console.log(answers);
+    res.render("question", {
+      answers,
+    });
+  })
+);
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const answers = db.Answer.findAll({ limit: 10 });
+  })
+);
 
 router.post(
-  "/",
-  restoreUser,
+  "/:id",
+  requireAuth,
   asyncHandler(async (req, res) => {
-    const userId = res.locals.userId;
-    const { content } = req.body;
-    db.Answer.build({ userId, content });
+    const questionId = req.params.id;
+    const userId = res.locals.user.id;
+    const { answerContents, questionId } = req.body;
+    console.log(answerContents);
+    const answer = db.Answer.build({
+      userId,
+      content: answerContents,
+      questionId,
+    });
+    await answer.save();
+    res.redirect("/api/answers/test");
   })
 );
 
