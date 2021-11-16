acb2e812849809830f0828e783fb0c6e9610e5ec
cameron <cam@loudstyle.com>
2021-11-15 18:35:19 -0600

    questionsPage

--- routes/questions.js
+++ routes/questions.js
@@ -2,23 +2,40 @@
 const router = require("express").Router();
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
-const { questionValidation } = require('./validations');
+const { questionValidation } = require("./validations");
 
 // route GET /questions/new => render new question form
-router.get("/questions/new", csrfProtection, (req, res) => {
+router.get("/new", csrfProtection, (req, res) => {
   const question = db.Question.build();
-  res.render('question-create', { question, csrfToken: req.csrfToken() });
+  res.render("question-create", { question, csrfToken: req.csrfToken() });
 });
 
-router.post("/questions/new", csrfProtection, asyncHandler((req, res) => {
-  const { content, title } = req.body;
-  const user = res.locals.user;
+router.post(
+  "/new",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const { content, title } = req.body;
+    const user = res.locals.user;
 
-  await db.Question.create({
-    title,
-    content,
-    user: user.id
-  });
-}));
+    await db.Question.create({
+      title,
+      content,
+      user: user.id,
+    });
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const questionId = req.params.id;
+    const question = await db.Question.findByPk(questionId);
+    if (!question) next(new Error("Question not found"));
+    const answers = await db.Answer.findAll({
+      where: { questionId: questionId },
+    });
+    res.render("question", { question, answers });
+  })
+);
 
 module.exports = router;
