170ad8fcb089ea705f57bc654833b646e36e0a8a
walceus <widalceus@gmail.com>
2021-11-16 12:21:45 -0500

    debugging

--- routes/profile.js
+++ routes/profile.js
@@ -1,23 +1,50 @@
-const router = express.Router();
 const express = require("express");
-const { requireAuth } = require("../../auth");
-const db = require("../../db/models");
-const { asyncHandler } = require("../utils");
+const { requireAuth } = require("../auth");
+const db = require("../db/models");
+const { asyncHandler } = require("./utils");
+const 
+const router = express.Router();
 
 
+console.log("HI")
 router.get(
-    '/users/:id(\\d+)',
+    '/:id(\\d+)',
     requireAuth,
     asyncHandler(async(req, res) => {
-    const user = await db.user.FindByPk(userId, {include: [{model: db.questions}]})
+    console.log("Hii")
+    const user = await db.User.findByPk(userId, {include: [{model: db.Question}, {model: db.Answer}]})
     let userId = undefined
     if (req.session.user) {
         userId = req.session.user.userId
-     }
+    }
     res.render('profile-page', {
         user,
         title,
         content
+    })
+}))
+
+router.post()
+
+router.put()
+
+router.delete()
+router.get(
+    'profiles/:id(\\d+)',
+    requireAuth,
+    asyncHandler(async(req, res) => {
+    const id = req.params.id;
+    const user = await User.findAll({
+        include: [{model: Question}, {model: Answer}],
+        where: {
+            userId: id
+        }
+    })
+    // if (req.session.user) {
+    //     userId = req.session.user.userId
+    // }
+    res.render('profile-page', {
+
     })
 }))
 
