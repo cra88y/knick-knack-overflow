01a297754b1567ae5b2478c2e999a52b149d591b
Tommy Forde <tommyforde@Tommys-MacBook-Pro.local>
2021-11-16 10:41:17 -0600

    made progress on create question functionality

--- routes/questions.js
+++ routes/questions.js
@@ -1,24 +1,47 @@
 // imports
 const router = require("express").Router();
 const db = require("../db/models");
-const { csrfProtection, asyncHandler } = require("./utils");
-const { questionValidation } = require('./validations');
+const { questionValidators } = require('./validations');
+const {
+  csrfProtection,
+  asyncHandler,
+  validationCheck
+} = require("./utils");
 
 // route GET /questions/new => render new question form
-router.get("/questions/new", csrfProtection, (req, res) => {
+router.get("/new", csrfProtection, (req, res) => {
   const question = db.Question.build();
-  res.render('question-create', { question, csrfToken: req.csrfToken() });
+  res.render('question-create', { title: "Ask Question", question, csrfToken: req.csrfToken() });
 });
 
-router.post("/questions/new", csrfProtection, asyncHandler((req, res) => {
-  const { content, title } = req.body;
-  const user = res.locals.user;
+router.post(
+  "/new",
+  csrfProtection,
+  questionValidators,
+  validationCheck,
+  asyncHandler(async (req, res) => {
+    const { content, title } = req.body;
+    const user = res.locals.user;
+    const errors = req.errors.errors;
 
-  await db.Question.create({
-    title,
-    content,
-    user: user.id
-  });
-}));
+    if (!errors.length) {
+      // validations pass
+      await db.Question.create({
+        title,
+        content,
+        user: user.id
+      });
+
+      res.redirect("/");
+    } else {
+      // validations don't pass
+      res.render("question-create", { 
+        title: "Ask Question",
+        question: req.body, 
+        csrfToken: req.csrfToken(),
+        errors: errors.map(err => err.msg)
+      }); 
+    }
+  }));
 
 module.exports = router;
