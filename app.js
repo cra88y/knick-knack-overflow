ba34b31d872caab0de57d48c48c3412865e23913
tforde4623 <sltebr33zy@gmail.com>
2021-11-16 10:41:17 -0600

    made progress on create question functionality

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const answersRouter = require("./routes/api/answers");
+const questionRouter = require("./routes/questions");
 const { restoreUser } = require("./auth");
 const { sessionSecret } = require("./config/index");
 
@@ -43,6 +44,7 @@ app.use(restoreUser);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/questions", questionRouter)
 app.use("/api/answers", answersRouter);
 
 // catch 404 and forward to error handler
