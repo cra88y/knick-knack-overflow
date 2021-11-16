2da6975c1242c861e17846141065cc4526fa7c9d
walceus <widalceus@gmail.com>
2021-11-15 22:00:07 -0500

    Profile Page

---
+++ routes/profile.js
@@ -0,0 +1,19 @@
+const router = express.Router();
+const express = require("express");
+const { requireAuth } = require("../../auth");
+const db = require("../../db/models");
+const { asyncHandler } = require("../utils");
+
+
+router.get('/users/:id(\\d+)', requireAuth, asyncHandler(async(req, res) => {
+    const user = await db.user.FindByPk(userId, {include: [{model: db.questions}]})
+    let userId = undefined
+    if (req.session.user) {
+        userId = req.session.user.userId
+     }
+    res.render('profile-page', {
+        user,
+        title,
+        content
+    })
+}))
