37c1ff5d54ad2fc495b39911b9e5d685af3ff675
cameron <cam@loudstyle.com>
2021-11-17 13:17:08 -0600

    Delete answers and updated favicon

--- routes/api/answers.js
+++ routes/api/answers.js
@@ -21,15 +21,29 @@ router.get(
     const answers = db.Answer.findAll({ limit: 10 });
   })
 );
-
 router.post(
-  "/:id",
+  "/answers/:id/delete",
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const answerId = req.params.id;
+    const userId = res.locals.user.id;
+    const answer = await db.Answer.findByPk(answerId);
+    if (userId != answer.userId) {
+      return next(
+        new Error("You are not the author of this answer. Nice try.")
+      );
+    }
+    answer.destroy();
+    res.redirect("back");
+  })
+);
+router.post(
+  "/questions/:id/answers",
   requireAuth,
   asyncHandler(async (req, res) => {
     const questionId = req.params.id;
     const userId = res.locals.user.id;
     const { answerContents } = req.body;
-    console.log(answerContents);
     const answer = db.Answer.build({
       userId,
       content: answerContents,
@@ -49,7 +63,7 @@ router.post(
     // console.log('here000000000')
     let { answerId } = req.body;
     let voteType = true;
-    answerId = parseInt(answerId, 10)
+    answerId = parseInt(answerId, 10);
     // userId = parseInt(userId, 10)
 
     // console.log('000000000000000000 userId', typeof(userId), userId)
@@ -57,21 +71,20 @@ router.post(
     // console.log('VOTE:', db.Vote)
     // try {
 
-      const voteStatus = await db.Vote.findOne({
-        where: {
-          userId,
-          answerId
-        }
-      })
-      console.log('0000000000 DB voteType', voteStatus.voteType)
-      if (voteStatus.voteType === true) voteType = false
-      else voteType = true
+    const voteStatus = await db.Vote.findOne({
+      where: {
+        userId,
+        answerId,
+      },
+    });
+    console.log("0000000000 DB voteType", voteStatus.voteType);
+    if (voteStatus.voteType === true) voteType = false;
+    else voteType = true;
     // } catch (err) {
     //   voteType = true;
     // }
-    console.log("000000000000TIME", voteStatus.createdAt)
+    console.log("000000000000TIME", voteStatus.createdAt);
     // console.log('00000000000 CATCH')
-
 
     //MAKE THE VOTE
 
