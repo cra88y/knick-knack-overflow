1c1767b7727690dc6d5d1d3c4c42ea8d73187c24
cameron <cam@loudstyle.com>
2021-11-18 17:20:59 -0600

    votes

--- routes/questions.js
+++ routes/questions.js
@@ -123,8 +123,6 @@ router.get(
     answers.sort((f, s) => {
       return s.voteCount - f.voteCount;
     });
-    console.log("000000000000000000000");
-    console.log(answers);
 
     //////////////////////////////////
     //populate votes:
@@ -161,23 +159,28 @@ router.get(
 
     // get suggested other questions
     // go through current question title and grab all words seperately
-    const titleTerms = question.title.split(' ');
+    const titleTerms = question.title.split(" ");
     const suggested = await db.Question.findAll({
       where: {
-        [Op.or]: [...titleTerms.map(term => {
-          // using terms longer than 2 chars to try and target more topic specific words
-          if (term.length > 3) {
-            return { title: { [Op.iLike]: `%${term}%` } }
-          }
-        })]
+        [Op.or]: [
+          ...titleTerms.map((term) => {
+            // using terms longer than 2 chars to try and target more topic specific words
+            if (term.length > 3) {
+              return { title: { [Op.iLike]: `%${term}%` } };
+            }
+          }),
+        ],
       },
       limit: 10,
-      order: [
-        ['createdAt', 'DESC']
-      ]
+      order: [["createdAt", "DESC"]],
     });
 
-    res.render("question", { suggested, question, answers, csrfToken: req.csrfToken() });
+    res.render("question", {
+      suggested,
+      question,
+      answers,
+      csrfToken: req.csrfToken(),
+    });
   })
 );
 
@@ -225,7 +228,6 @@ router.get(
     res.status(201).json({
       answerVotes,
       userVotes,
-
     });
   })
 );
