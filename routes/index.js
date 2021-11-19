775c30cae382a26f6cc5da4d0bda1b44e5d2f6be
Tommy Forde <tommyforde@Tommys-MacBook-Pro.local>
2021-11-19 15:11:10 -0600

    added ability for pages

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,42 @@ const { asyncHandler } = require("./utils");
 
 /* GET home page. */
 router.get(
-  "/",
+  "/:pageNum?",
   asyncHandler(async (req, res) => {
-    const questions = await db.Question.findAll({
-      limit: 10,
-      raw: true,
-      include: { model: db.User, attributes: ["username", "id"] },
-      order: [
-        ['createdAt', 'DESC']
-    ],
-    });
+    const numPages = Math.ceil(await db.Question.count() / 10);
+    const pageLinks = [];
+
+    for (let i = 1; i <= numPages; i++) {
+      pageLinks.push(i);
+    }
+
+    let questions;
+
+    if (req.params.pageNum) {
+      questions = await db.Question.findAll({
+        offset: ((req.params.pageNum - 1) * 10),
+        limit: 10,
+        raw: true,
+        include: { model: db.User, attributes: ["username", "id"] },
+        order: [
+          ['createdAt', 'DESC']
+        ],
+      });
+    } else {
+      questions = await db.Question.findAll({
+        raw: true,
+        include: { model: db.User, attributes: ["username", "id"] },
+        order: [
+          ['createdAt', 'DESC']
+        ],
+      });
+    }
+
+
+
     res.render("index", {
       questions,
+      pageLinks
     });
   })
 );
@@ -27,4 +51,5 @@ router.get(
     res.render("about-us")
   }
 )
+
 module.exports = router;
